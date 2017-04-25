596c69b28fce00fbf5249273ca8983dda9f3c6f9
Hao Luo <luohao.brian@gmail.com>
2017-04-25 16:53:15 +0800

    Create job_show.sh

---
+++ job_show.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PROJECT_ID='c75709169d574f08b403ffc439a4e8e1'
+NAME=''
+PASSWD=''
+JOB_ID='8aace0c75b8f1654015ba4231e972301'
+
+TOKEN_BODY='{
+  "auth": {
+    "identity": {
+      "methods": [
+        "password"
+      ],
+      "password": {
+        "user": {
+          "name": '"\"$NAME\""',
+          "password": '"\"$PASSWD\""',
+          "domain": {
+            "name": '"\"$NAME\""'
+          }
+        }
+      }
+    },
+   "scope": {
+      "project": {
+        "name": "cn-north-1"
+      }
+    }
+  }
+}'
+
+TOKEN=`curl -i -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY" | grep "X-Subject-Token"| awk '{print$2}'`
+
+curl -i -X GET https://ecs.cn-north-1.myhwclouds.com/v1/${PROJECT_ID}/jobs/${JOB_ID} -H "X-Auth-Token:${TOKEN}"
