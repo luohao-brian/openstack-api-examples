550a930f8cf63ee082bb804c9fea6ff1c6ccee30
Hao Luo <luohao.brian@gmail.com>
2017-04-25 16:59:19 +0800

    Create vpc_list.sh

---
+++ vpc_list.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PROJECT_ID='c75709169d574f08b403ffc439a4e8e1'
+NAME='hwcloud5967'
+PASSWD='Huawei@123'
+
+TOKEN_BODY='{
+  "auth": {
+    "identity": {
+      "methods": [
+        "password"
+      ],
+      "password": {
+        "user": {
+          "name": '"\"$NAME\""',
+          "password": '"\"$PASSWD\""',
+          "domain": {
+            "name": '"\"$NAME\""'
+          }
+        }
+      }
+    },
+   "scope": {
+      "project": {
+        "name": "cn-north-1"
+      }
+    }
+  }
+}'
+
+TOKEN=`curl -i -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY" | grep "X-Subject-Token"| awk '{print$2}'`
+
+curl -i -X GET https://vpc.cn-north-1.myhwclouds.com/v1/${PROJECT_ID}/vpcs -H "X-Auth-Token:${TOKEN}"
