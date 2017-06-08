5c8ff91a3c91d53ca632b7fc672094fafaea79f3
Hao Luo <luohao.brian@gmail.com>
2017-06-09 00:16:50 +0800

    Create vm_detail.sh

--- vm_detail.sh
+++ vm_detail.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-PROJECT_ID='c75709169d574f08b403ffc439a4e8e1'
 NAME=''
 PASSWD=''
 SERVER_ID='a23d5aff-af66-4a9e-a542-bff7991ef62d'
@@ -30,5 +29,6 @@ TOKEN_BODY='{
 }'
 
 TOKEN=`curl -i -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY" | grep "X-Subject-Token"| awk '{print$2}'`
+PROJECT_ID=`curl -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY"| python -mjson.tool| grep -A 3 project| grep id|awk -F "\"" '{print $4}'`
 
 curl -i -X GET https://ecs.cn-north-1.myhwclouds.com/v2/${PROJECT_ID}/servers/${SERVER_ID} -H "X-Auth-Token:${TOKEN}"
