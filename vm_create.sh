79c571d1877077f511780c648a8ae0ccdd820940
Hao Luo <luohao.brian@gmail.com>
2017-06-09 00:15:35 +0800

    Create vm_create.sh

--- vm_create.sh
+++ vm_create.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-PROJECT_ID='c75709169d574f08b403ffc439a4e8e1'
 NAME=''
 PASSWD=''
 
@@ -29,6 +28,7 @@ TOKEN_BODY='{
 }'
 
 TOKEN=`curl -i -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY" | grep "X-Subject-Token"| awk '{print$2}'`
+PROJECT_ID=`curl -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY"| python -mjson.tool| grep -A 3 project| grep id|awk -F "\"" '{print $4}'`
 
 CREATE_VM_BODY='{
     "server": {
