8008f4526d03a04a9d0734cd3310a9c67f600b66
Hao Luo <luohao.brian@gmail.com>
2017-06-11 22:34:57 +0800

    Create vm_detail.sh

--- vm_detail.sh
+++ vm_detail.sh
@@ -4,7 +4,7 @@ NAME=''
 PASSWD=''
 SERVER_ID='a23d5aff-af66-4a9e-a542-bff7991ef62d'
 
-TOKEN_BODY='{
+AUTH_PARAMS='{
   "auth": {
     "identity": {
       "methods": [
@@ -28,7 +28,7 @@ TOKEN_BODY='{
   }
 }'
 
-TOKEN=`curl -i -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY" | grep "X-Subject-Token"| awk '{print$2}'`
-PROJECT_ID=`curl -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY"| python -mjson.tool| grep -A 3 project| grep id|awk -F "\"" '{print $4}'`
+TOKEN=`curl -i -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$AUTH_PARAMS" | grep "X-Subject-Token"| awk '{print$2}'`
+PROJECT_ID=`curl -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$AUTH_PARAMS"| python -mjson.tool| grep -A 3 project| grep id|awk -F "\"" '{print $4}'`
 
 curl -i -X GET https://ecs.cn-north-1.myhwclouds.com/v2/${PROJECT_ID}/servers/${SERVER_ID} -H "X-Auth-Token:${TOKEN}"
