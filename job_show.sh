5d93466271b696822aff985718bdbab1e4dca5b8
Hao Luo <luohao.brian@gmail.com>
2017-06-11 22:29:59 +0800

    Create job_show.sh

--- job_show.sh
+++ job_show.sh
@@ -4,7 +4,7 @@ NAME=''
 PASSWD=''
 JOB_ID='8aace0c75b8f1654015ba4231e972301'
 
-TOKEN_BODY='{
+AUTH_PARAMS='{
   "auth": {
     "identity": {
       "methods": [
@@ -28,7 +28,7 @@ TOKEN_BODY='{
   }
 }'
 
-TOKEN=`curl -i -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY" | grep "X-Subject-Token"| awk '{print$2}'`
-PROJECT_ID=`curl -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$TOKEN_BODY"| python -mjson.tool| grep -A 3 project| grep id|awk -F "\"" '{print $4}'`
+TOKEN=`curl -i -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$AUTH_PARAMS" | grep "X-Subject-Token"| awk '{print$2}'`
+PROJECT_ID=`curl -X POST   https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens  -H 'content-type: application/json'   -d "$AUTH_PARAMS"| python -mjson.tool| grep -A 3 project| grep id|awk -F "\"" '{print $4}'`
 
 curl -i -X GET https://ecs.cn-north-1.myhwclouds.com/v1/${PROJECT_ID}/jobs/${JOB_ID} -H "X-Auth-Token:${TOKEN}"
