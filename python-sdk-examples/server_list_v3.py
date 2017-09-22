be4a1b53e9e6b9d0727d726d1c9f3f8ee2225354
Luo Hao <luohao.brian@gmail.com>
2017-09-22 22:59:14 +0800

    Simplify python example

--- python-sdk-examples/server_list_v3.py
+++ python-sdk-examples/server_list_v3.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
+
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
 from novaclient import client
-from credentials import get_nova_credentials_v3
 
-credentials = get_nova_credentials_v3()
-auth = v3.Password(**credentials)
+auth = v3.Password(
+    	username='HEC_USER_NAME',
+    	password='HEC_USER_PASSWD',
+    	auth_url='https://iam.cn-north-1.myhwclouds.com/v3',
+    	user_domain_name='HEC_USER_NAME',
+    	project_domain_name='HEC_USER_NAME'
+)
 sess = session.Session(auth=auth, verify=False)
 nova = client.Client("2.1", session=sess)
 print nova.servers.list()
